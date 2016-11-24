0db31cac61ef2ec9b436ca255c8af64a1d2ef109
sunyiqian <877203755@qq.com>
2016-11-24 13:22:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-define('APPID','wx51068f33468988d2');
-define('APPSECRET','cb39814033ab87fbce4a8a9d8c724ad8');
+define('APPID','wxcc6bb2a0b743bf5d');
+define('APPSECRET','5baa74f3fa748db70b8fd47ba7f5fd6b');
 define('TOKEN','weixinopen');
 
 require './wechat.class.php';
 $wechat = new WeChat(APPID, APPSECRET, TOKEN);
-$wechat->responseMsg();//处理从微信平台发送的信息
+$wechat->valid();//处理从微信平台发送的信息
 ?>
